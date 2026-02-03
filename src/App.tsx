c99d6d1bb54d8f73b387fc48ae1620b080cda75e
Sahil <sahiltaneja07@gmail.com>
2026-02-03 23:48:31 +0000

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <div>
-      Mat Chip Selector
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
